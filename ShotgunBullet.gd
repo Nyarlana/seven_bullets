ce9b41cb7487c76f972ee397db6ebe397a9a2e46
Ersikan <julien.philippon@epitech.eu>
2021-04-11 18:17:03 +0200

    Add shotgun

---
+++ ShotgunBullet.gd
@@ -0,0 +1,20 @@
+extends Bullet
+
+export var rotation_speed := PI * 2
+export var speed = 500
+export var lifetime := 2
+var velocity := Vector2(speed, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	gravity = 800
+	velocity = velocity.rotated(rotation)
+	yield(get_tree().create_timer(lifetime), "timeout")
+	destroy()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity.y += gravity * delta
+	position += velocity * delta
+
